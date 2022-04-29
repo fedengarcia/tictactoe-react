b1214662f778b33d4776a1bc075434f92b077b88
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 12:28:33 +0200

    Create GameTablero.js

---
+++ src/components/GameTableroComponent/GameTablero.js
@@ -0,0 +1,15 @@
+import GameTableroBox from "../GameTableroBoxComponent/GameTableroBox";
+
+
+
+export default function GameTablero ({handlePlay,tablero}) {
+
+    return (
+        <div className="tablero-container">
+            <div className="tablero-grid">
+                {tablero.map((tablerobox,i) =>
+                 <GameTableroBox key={i} position={i} handlePlay={handlePlay} value={tablero[i]}/>)}
+            </div>
+        </div>
+    )
+}
