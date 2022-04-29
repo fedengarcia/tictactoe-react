5d171ff18cb12c6c494e08ae70cbe7361f2f50e9
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 12:28:58 +0200

    Update Router.js

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Route,Routes,BrowserRouter} from 'react-router-dom';
-import GameModeContainer from '../components/GameMode/GameModeContainer';
+import GameModeContainer from '../components/GameModeComponent/GameModeContainer';
 import RankingContainer from '../components/RankingContainer/RankingContainer';
 import TicTacToe from '../components/TicTacToe/TicTacToe';
 import PlayerInfoContainer from '../components/PlayerInfo/PlayerInfoContainer';
