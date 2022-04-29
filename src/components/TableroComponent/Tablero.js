569bb0edc15d81f7fc1b3d02268a5e396a1bf3f5
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 08:41:43 +0200

    Tablero

---
+++ src/components/TableroComponent/Tablero.js
@@ -0,0 +1,15 @@
+
+
+
+
+export default function Tablero ({players}) {
+
+
+
+
+    return (
+        <div className="tablero-container">
+
+        </div>
+    )
+}
--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -1,17 +1,20 @@
 import React, { useState, useContext } from 'react';
 import { UseGameContext } from '../../context/GameContext';
+import StartGameView from '../StartGameComponent/StartGameView';
+import Tablero from '../TableroComponent/Tablero';
 
 export default function TicTacToe () {
     const [play, setPlay] = useState(false);
     const {players,gameType} = useContext(UseGameContext)
 
-    return (<div>
+    return (
+    <div className="game-container">
        <h1>{gameType}</h1>
-        {/* {play === false
-         ? <StartGame setPlay={setPlay}/>
-         : <Tablero />
+        {play === false
+         ? <StartGameView setPlay={setPlay}/>
+         : <Tablero players={players}/>
 
-        } */}
+        }
 
     </div>
     )
