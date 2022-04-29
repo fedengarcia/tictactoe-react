f663de2ab8f284abd7d16d3396bbff16f7f4bb75
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 12:28:06 +0200

    Create GamePlayer.js

---
+++ src/components/GamePlayersComponent/GamePlayer.js
@@ -0,0 +1,24 @@
+
+
+
+export default function GamePlayer ({players, gameType}) {
+
+
+    return (
+        <div className="players-container">
+                {gameType === "Computer" 
+                ? <>
+                    <h2>{`Player O: ${players.playerOne.name}`}</h2>
+                    <h2>VS</h2>
+                    <h2>computadora</h2>
+                </>
+                : <>
+                    <h2>{`Player O: ${players.playerOne.name}`}</h2>
+                    <h2>VS</h2>
+                    <h2>{`Player X: ${players.playerTwo.name}`}</h2>
+                </>
+                }
+            
+        </div>
+    )
+}
