2f66868fb148825545db71377b8b4930c0808e2e
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 14:59:37 +0200

    Mostrar ganador y puntos o empate

--- src/components/GameFinishComponent/GameFinishView.js
+++ src/components/GameFinishComponent/GameFinishView.js
@@ -3,16 +3,23 @@
 
 
 
-export default function GameFinishView ({setPlay}) {
+export default function GameFinishView ({setPlay, setTablero,tablero, winner}) {
 
     const handlePlay = () =>{
+        setTablero(tablero.fill(""))
+        // GUARDAR EN FIREBASE PUNTOS DEL GANADOR
         setPlay(true);
     }
 
     return (
         <div className="game-finish-container">
+            {winner.name && 
+                <><h2>{`Ganador: ${winner.name}`}</h2>
+                <h2>{`Haz sumado: ${winner.points} puntos a tu historial`}</h2>
+                </>}
+            {!winner.name && <h2>Empate</h2>}
             <button onClick={handlePlay}>Jugar de nuevo</button>
-            <button>Reglas de puntos</button>
+            <button>Ranking</button>
         </div>
     )
 }
