b69bdaca84ac59f9676daa31865efb0a86b2242f
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 12:28:12 +0200

    Create GameTimer.js

---
+++ src/components/GameTimerComponent/GameTimer.js
@@ -0,0 +1,13 @@
+
+
+
+export default function GameTimer ({timer,setTimer}) {
+
+
+
+    return (
+        <div className="timer-container">
+                <h2>Timer: </h2>
+            </div>
+    )
+}
