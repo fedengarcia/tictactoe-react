bfadea604572981dab7be6fb48073e80fc54cde7
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 15:00:22 +0200

    Update TicTacToe.js

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -5,15 +5,18 @@ import GameFinishView from '../GameFinishComponent/GameFinishView';
 import GameTurno from '../GameTurnoComponent/GameTurno';
 import GameTimer from '../GameTimerComponent/GameTimer';
 import GamePlayer from '../GamePlayersComponent/GamePlayer';
+import { checkWinner } from '../helper';
+
 
 export default function TicTacToe () {
     const [play, setPlay] = useState(true);
     const [timer,setTimer] = useState(undefined);
     const [turnoJugador,setTurnoJugador] = useState("");
     const [tablero,setTablero] = useState(["","","","","","","","",""])
-   
-    const {players,gameType} = useContext(UseGameContext)
-    
+
+    const {players,gameType,} = useContext(UseGameContext)
+    const winner = checkWinner(tablero,players);
+
     useEffect(() => {
         
         if (Math.floor(Math.random()*["O","X"].length) === 0){
@@ -25,40 +28,48 @@ export default function TicTacToe () {
     }, []);
 
 
+    useEffect(() => {
+        if(winner){
+            setPlay(false);
+        }
+    }, [winner]);
+
+
+
     const handlePlay = (position) => {
         const tableroCopy =  tablero.slice();
-        tableroCopy[position] =  turnoJugador;
-        setTablero(tableroCopy);
+        if(tableroCopy[position] === ""){
+            tableroCopy[position] =  turnoJugador;
+            setTablero(tableroCopy);
 
-        if(turnoJugador === "X"){
-            setTurnoJugador("O");
-            console.log("Le toca jugar a ",turnoJugador);
-        }else{
-            setTurnoJugador("X");
-            console.log("Le toca jugar a ",turnoJugador);
-
+            if(turnoJugador === "X"){
+                setTurnoJugador("O");
+            }else{
+                setTurnoJugador("X");
+            }
         }
-
+        
+        
     }
 
     return (
-    <div className="game-container">
-       <h1>Tipo de juego: {gameType}</h1>
-        {play === false
-         ? <GameFinishView setPlay={setPlay}/>
-         : <>
+        <div className="game-container">
+        <h1>Tipo de juego: {gameType}</h1>
+            {play === false
+            ? <GameFinishView setPlay={setPlay} winner={winner} tablero={tablero} setTablero={setTablero}/>
+            : <>
+                
+                <GameTimer timer={timer} setTimer={setTimer}/>
+                
+                <GamePlayer players={players} gameType={gameType}/>
             
-            <GameTimer timer={timer} setTimer={setTimer}/>
+                <GameTablero handlePlay={handlePlay} tablero={tablero}/>
+                
+                <GameTurno players={players} gameType={gameType} turnoJugador={turnoJugador}/>
             
-            <GamePlayer players={players} gameType={gameType}/>
-        
-            <GameTablero handlePlay={handlePlay} tablero={tablero}/>
-            
-            <GameTurno players={players} gameType={gameType} turnoJugador={turnoJugador}/>
-         
-         </>
-        }
+            </>
+            }
 
-    </div>
+        </div>
     )
 }
