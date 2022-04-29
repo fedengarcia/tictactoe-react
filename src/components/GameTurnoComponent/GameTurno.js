e28dd14837b071ce78721bd08d556d4efe89d171
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 12:28:14 +0200

    Create GameTurno.js

---
+++ src/components/GameTurnoComponent/GameTurno.js
@@ -0,0 +1,15 @@
+
+
+
+export default function GameTurno ({gameType,players,turnoJugador}) {
+
+    return (
+        <div className="turno-container">
+            {gameType === "Multiplayer" ? 
+            <h2>{`Turno de: ${turnoJugador === "O" ? players.playerOne.name : players.playerTwo.name}`}</h2>
+            :
+            <h2>{`Turno de: ${turnoJugador === "O" ? players.playerOne.name : "Computadora"}`}</h2>
+            }
+        </div>
+    )
+}
