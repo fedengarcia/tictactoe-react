ade436e483fde1914604a215145a22fbd8d9bb90
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-29 12:29:26 +0200

    Funcionalidad de clicks y turnos

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -1,20 +1,62 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { UseGameContext } from '../../context/GameContext';
-import Tablero from '../Tablero/Tablero';
+import GameTablero from '../GameTableroComponent/GameTablero';
 import GameFinishView from '../GameFinishComponent/GameFinishView';
-
+import GameTurno from '../GameTurnoComponent/GameTurno';
+import GameTimer from '../GameTimerComponent/GameTimer';
+import GamePlayer from '../GamePlayersComponent/GamePlayer';
 
 export default function TicTacToe () {
     const [play, setPlay] = useState(true);
+    const [timer,setTimer] = useState(undefined);
+    const [turnoJugador,setTurnoJugador] = useState("");
+    const [tablero,setTablero] = useState(["","","","","","","","",""])
+   
     const {players,gameType} = useContext(UseGameContext)
     
-    
+    useEffect(() => {
+        
+        if (Math.floor(Math.random()*["O","X"].length) === 0){
+            setTurnoJugador("O");
+        }else{
+            setTurnoJugador("X");
+        }
+        
+    }, []);
+
+
+    const handlePlay = (position) => {
+        const tableroCopy =  tablero.slice();
+        tableroCopy[position] =  turnoJugador;
+        setTablero(tableroCopy);
+
+        if(turnoJugador === "X"){
+            setTurnoJugador("O");
+            console.log("Le toca jugar a ",turnoJugador);
+        }else{
+            setTurnoJugador("X");
+            console.log("Le toca jugar a ",turnoJugador);
+
+        }
+
+    }
+
     return (
     <div className="game-container">
        <h1>Tipo de juego: {gameType}</h1>
         {play === false
          ? <GameFinishView setPlay={setPlay}/>
-         : <Tablero players={players} gameType={gameType}/>
+         : <>
+            
+            <GameTimer timer={timer} setTimer={setTimer}/>
+            
+            <GamePlayer players={players} gameType={gameType}/>
+        
+            <GameTablero handlePlay={handlePlay} tablero={tablero}/>
+            
+            <GameTurno players={players} gameType={gameType} turnoJugador={turnoJugador}/>
+         
+         </>
         }
 
     </div>
