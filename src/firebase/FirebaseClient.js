3db1b9c724bfc4c15c3a1a04602d4edca9789190
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-05-01 15:11:05 +0200

    Estilos

--- src/firebase/FirebaseClient.js
+++ src/firebase/FirebaseClient.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
-import { query, collection, addDoc, getDocs, where,limit } from "firebase/firestore";
+import { query, collection, addDoc, getDocs, where } from "firebase/firestore";
 
 
 
