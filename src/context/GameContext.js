d394294468722199b929e8cc19ec68391c86bfab
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-05-01 22:54:48 +0200

    Update GameContext.js

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -17,28 +17,30 @@ export function GameContext ({children}) {
 
 
     //DEVUELVO -1 SI NO EXISTE EL ITEM
-    const getWinnerIndex = (name,gameType) =>{
-        const position = winnersRanking.findIndex(item => (item.name === name && item.gameType === gameType)); //&& item.playerType === playerType
+    const getWinnerIndex = (name) =>{
+        const position = winnersRanking.findIndex(item => (item.name === name)); //&& item.playerType === playerType
         return position;
     }
 
     //MODELO DATOS PARA EL RANKING DE GANADORES
     const modelDataRanking = (winner) => {
-
-        let result = getWinnerIndex(winner.name,winner.gameType);
-        //NO EXISTE ELEMENTO
-        if(result === -1){
-            winnersRanking.push(winner);
-            // setWinnersRanking(winnersRanking => [...winnersRanking,winner])
-
-            // EXISTE, ACTUALIZO DATOS Y ORDENO ARRAY
-        }else{
-            const winnersRankingCopy = [...winnersRanking];
-            winnersRankingCopy[result]["points"] = winnersRankingCopy[result]["points"] + winner.points;
-            winnersRankingCopy[result]["wins"] = winnersRankingCopy[result]["wins"] + 1;
-            setWinnersRanking(winnersRankingCopy.sort((a, b) => (a.points < b.points) ? 1 : -1)
-            )
+        if(winner.gameType === "Multiplayer"){
+            let result = getWinnerIndex(winner.name);
+            //NO EXISTE ELEMENTO
+            if(result === -1){
+                winnersRanking.push(winner);
+                // setWinnersRanking(winnersRanking => [...winnersRanking,winner])
+    
+                // EXISTE, ACTUALIZO DATOS Y ORDENO ARRAY
+            }else{
+                const winnersRankingCopy = [...winnersRanking];
+                winnersRankingCopy[result]["points"] = winnersRankingCopy[result]["points"] + winner.points;
+                winnersRankingCopy[result]["wins"] = winnersRankingCopy[result]["wins"] + 1;
+                setWinnersRanking(winnersRankingCopy.sort((a, b) => (a.points < b.points) ? 1 : -1)
+                )
+            }
         }
+        
     }
 
     // VACIO RANKING
